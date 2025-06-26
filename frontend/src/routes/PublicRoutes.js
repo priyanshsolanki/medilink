fa4b8356ac32bf48e466b23c30acc1207a6ee40c
Arsh255 <arshadvhora512@gmail.com>
2025-06-26 04:16:57 -0300

    Feature(Signup pages): create Sigup page for both patient and doctor with all formik validation and required UI components

--- frontend/src/routes/PublicRoutes.js
+++ frontend/src/routes/PublicRoutes.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
+import Home from "../pages/HomePage/Home";
+import LoginPage from "../pages/Auth/LoginPage";
+import PatientRegister from "../pages/Auth/PatientRegister";
+import DoctorRegister from "../pages/Auth/DoctorRegister";
 
 const PublicRoutes = () => {
   return (
     <Routes>
-       {/* <Route exact path="/" element={<Login />} />  */}
+       <Route exact path="/" element={<Home />} /> 
+       <Route exact path="/login" element={<LoginPage />} /> 
+       <Route exact path="/patientRegister" element={<PatientRegister />} /> 
+       <Route exact path="/doctorRegister" element={<DoctorRegister />} /> 
+
+      
     </Routes>
   );
 };
