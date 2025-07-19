5141d8da369f107085425bcb5de4a7f2ccf954ff
alwinsanil <al589400@dal.ca>
2025-07-18 22:04:38 -0300

    Appointments module Implemented

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.use(passport.session());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/availability', require('./routes/availability'));
+app.use('/api/appointments', require('./routes/appointments'));
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
